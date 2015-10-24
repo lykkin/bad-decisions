0b2a6e238fc51729c98d60bc385d48fd2ba6ed9a
lykkin <bclement01@gmail.com>
2015-10-23 17:20:10 -0700

    abba zabba

--- header.php
+++ header.php
@@ -8,8 +8,19 @@
   ga('send', 'pageview');
 
 </script>
+<script type="text/x-mathjax-config">
+    MathJax.Hub.Config({
+        tex2jax: {
+            inlineMath: [
+                ['$','$'],
+                ['\\(','\\)']
+            ],
+            processEscapes: true
+        }
+    });
+</script>
 <script type="text/javascript"
-  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
 </script>
 
 <h1>((((((((((((((((((((</h1>
